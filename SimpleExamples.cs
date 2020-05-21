0929ce30a207d38d8d7d2417623d272a9bd1a1bb
mahmud-sharipov <32417702+mahmud-sharipov@users.noreply.github.com>
2020-05-21 14:14:03 +0500

    Update SimpleExamples.cs

--- SimpleExamples.cs
+++ SimpleExamples.cs
@@ -1,121 +1,116 @@
-public class TestNamingExample : TestBase
+using Eagle.Framework.Server.Entity;
+using Eagle.Framework.Server.Plugin;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Sales.Payment
 {
-	[Compute(SAStudent.pn.FullName, new string[] { SAStudent.pn.Name, SAStudent.pn.LastName }, UserSummary = "")]
-	public void SAStudent_FullName_Compute(SAStudent entity, ComputeEventArgs<string> e)
-	{
-	e.NewValue = $"{entity.Name} {entity.LastName}";
-	}
+    public class TestNamingExample : TestBase
+    {
+        #region compute
+        [Compute(SAStudent.pn.FullName, new string[] { SAStudent.pn.Name, SAStudent.pn.LastName }, UserSummary = "")]
+        public void SAStudent_FullName_Compute(SAStudent entity, ComputeEventArgs<string> e)
+        {
+            e.NewValue = $"{entity.Name} {entity.LastName}";
+        }
 
-	/*
-	* Use case:
-	* Name is not empty but LastName is empty
-	* Name is empty but LastName is not empty
-	* Name and LastName is empty
-	* Name and LastName is not empty
-	*/
-	//UnitOfWork_StateUnderTest_ExpectedBehavior
-	[TestMethod]
-	public void SAStudentFullNameCompute_NameIsNotEmptyLastNameIsEmpty_ShouldBeEqualToName()
-	{
-	var student = new SAStudent(Context);
-	student.Name = "Brent";
-	student.LastName = string.Empty;
-	Assert.AreEqual("Brent", student.FullName);
-	}
+        /*
+         * Use case: 
+         * Name is not empty but LastName is empty
+         * Name is empty but LastName is not empty 
+         * Name and LastName is empty
+         * Name and LastName is not empty
+         */
+        //UnitOfWork_StateUnderTest_ExpectedBehavior
+        [TestMethod]
+        public void SAStudentFullNameCompute_NameIsNotEmptyLastNameIsEmpty_ShouldBeEqualToName()
+        {
+            var student = new SAStudent(Context);
+            student.Name = "Brent";
+            student.LastName = string.Empty;
+            Assert.AreEqual("Brent", student.FullName);
+        }
 
-	[TestMethod]
-	public void SAStudent_FullNameCompute_NameIsEmptyLastNameIsNotEmpty_ShouldBeEqualToLastName()
-	{
-	var student = new SAStudent(Context);
-	student.Name = string.Empty;
-	student.LastName = "Esh";
-	Assert.AreEqual("Esh", student.FullName);
-	}
+        [TestMethod]
+        public void SAStudentFullNameCompute_NameIsEmptyLastNameIsNotEmpty_ShouldBeEqualToLastName()
+        {
+            var student = new SAStudent(Context);
+            student.Name = string.Empty;
+            student.LastName = "Esh";
+            Assert.AreEqual("Esh", student.FullName);
+        }
 
-	[TestMethod]
-	public void SAStudent_FullNameCompute_NameAndLastNameIsEmpty_ShouldBeEmpty()
-	{
-	var student = new SAStudent(Context);
-	student.Name = string.Empty;
-	student.LastName = string.Empty;
-	Assert.AreEqual(string.Empty, student.FullName);
-	}
+        [TestMethod]
+        public void SAStudentFullNameCompute_NameAndLastNameIsEmpty_ShouldBeEmpty()
+        {
+            var student = new SAStudent(Context);
+            student.Name = string.Empty;
+            student.LastName = string.Empty;
+            Assert.AreEqual(string.Empty, student.FullName);
+        }
 
-	[TestMethod]
-	public void SAStudent_FullNameCompute_NameAndLastNameIsNotEmpty_ShouldBeEqualToNameAndLastName()
-	{
-	var student = new SAStudent(Context);
-	student.Name = "Brent";
-	student.LastName = "Esh";
-	Assert.AreEqual("Brent Esh", student.FullName);
-	}
+        [TestMethod]
+        public void SAStudentFullNameCompute_NameAndLastNameIsNotEmpty_ShouldBeEqualToNameAndLastName()
+        {
+            var student = new SAStudent(Context);
+            student.Name = "Brent";
+            student.LastName = "Esh";
+            Assert.AreEqual("Brent Esh", student.FullName);
+        }
+        #endregion
 
-	[Validate(SAStudent.pn.FullName, UserSummary = "")]
-	[MessageTemplate(0, SeverityLevel.Error, "Full name is wrong", "Full name should not contain numeric number", "Please enter correct full name")]
-	[MessageTemplate(1, SeverityLevel.Error, "Full name is wrong", "Full name length should not be grove then 100", "Please enter correct full name")]
-	public void SAStudent_FullName_Validate(SAStudent entity, ValidateEventArgs<string> e)
-	{
-	var result = IsFullNameValid(e.NewValue);
-	if (result >= 0)
-	{
-	entity.Message(e, (uint)result);
-	}
-	}
+        #region validation
+        [Validate(SAStudent.pn.FullName, UserSummary = "")]
+        [MessageTemplate(0, SeverityLevel.Error, "Full name is wrong", "Full name should not contain numeric number", "Please enter correct full name")]
+        [MessageTemplate(1, SeverityLevel.Error, "Full name is wrong", "Full name length should not be grove then 100", "Please enter correct full name")]
+        public void SAStudent_FullName_Validate(SAStudent entity, ValidateEventArgs<string> e)
+        {
+            var result = IsFullNameValid(e.NewValue);
+            if (result >= 0)
+            {
+                entity.Message(e, (uint)result);
+            }
+        }
 
-	static int IsFullNameValid(string fullName)
-	{
-	if (IsEmpty(fullName))
-	{
-	return 0;
-	}
+        static int IsFullNameValid(string fullName)
+        {
+            if (string.IsNullOrEmpty(fullName))
+                return 0;
 
-	if (ContainsNumeric(fullName))
-	{
-	return 1;
-	}
+            if (fullName.Length > 100)
+                return 1;
 
+            return -1;
+        }
 
-	return -1;
+        [TestMethod]
+        public void IsFullNameValid_IsEmpty_Zero()
+        {
+            //TODO: ...
+        }
 
-	bool ContainsNumeric(string value)
-	{
-	return true;
-	}
+        [TestMethod]
+        public void IsFullNameValid_NotEmptyAndContaindsNumeric_One()
+        {
+            //TODO: ...
+        }
 
-	bool IsEmpty(string value)
-	{
-	return string.IsNullOrEmpty(value);
-	}
-	}
+        [TestMethod]
+        public void IsFullNameValid_NotEmptyAndDoseNotContainsNumeric_NegativOne()
+        {
+            //TODO: ...
+        }
 
-	[TestMethod]
-	public void IsFullNameValid_IsEmpty_Zero()
-	{
+        [TestMethod]
+        public void SAStudentFulllNameValidate_NotValid_DisplayMessage()
+        {
+            //TODO: ...
+        }
 
-	}
-
-	[TestMethod]
-	public void IsFullNameValid_NotEmptyAndContaindsNumeric_One()
-	{
-
-	}
-
-	[TestMethod]
-	public void IsFullNameValid_NotEmptyAndDoseNotContainsNumeric_NegativOne()
-	{
-
-	}
-
-	[TestMethod]
-	public void SAStudentFulllNameValidate_NotValid_DisplayMessage()
-	{
-
-	}
-
-	[TestMethod]
-	public void SAStudentFulllNameValidate_Valid_NoMessage()
-	{
-
-	}
-
+        [TestMethod]
+        public void SAStudentFulllNameValidate_Valid_NoMessage()
+        {
+            //TODO: ...
+        }
+        #endregion
+    }
 }
